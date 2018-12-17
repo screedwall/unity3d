a75d91d0779eb7d8a8a0897c0eadb28f68892225
screedwall <avtulev@gmail.com>
2018-12-17 06:10:37 +0300

    Revert "UI Added"
    This reverts commit 922785649f060288e1385e242019c47462cfd480.

--- Assets/Grid.cs
+++ Assets/Grid.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Grid : MonoBehaviour {
     public Transform prefab;
+    public int GridSize;
 	// Use this for initialization
 	void Start () {
 		
@@ -14,7 +15,7 @@ public class Grid : MonoBehaviour {
 		
 	}
 
-    static public void CreateMainGrid(Transform prefab, Transform parent, int player, int GridSize=10)
+    static public void CreateMainGrid(Transform parent, int player)
     {
         float positionX = 0;
         float positionZ = 0;
@@ -33,4 +34,24 @@ public class Grid : MonoBehaviour {
             positionZ += 5.3f;
         }
     }
+
+
+    float positionX = -55f;
+        float positionZ = 0;
+        for (int i = 0; i < GridSize; i++)
+        {
+            for (int j = 0; j < GridSize; j++)
+            {
+                positionX += 5.3f;
+                Transform parent = GameObject.Find("GameObject").GetComponent<Transform>();
+                GameObject plane = Instantiate(prefab, parent).gameObject;
+                plane.name = string.Format("Cell[{0},{1}]", i, j);
+
+                plane.GetComponent<CellController>().id = i * GridSize + j;
+                plane.transform.position = new Vector3(positionX, 0, positionZ);
+            }
+            positionX = -55f;
+            positionZ += 5.3f;
+        }
+
 }
